91abef0f4112af79792db4c53e5e262ab0ff8aea
Sergey Nacharov <xkoredeus@gmail.com>
2019-10-28 12:53:43 +0400

    feat: adaptive

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -5,3 +5,18 @@ $(function() {
     touch : false
   });
 });
+$(function() {
+  $('.map__pop-btn').on('click', function() {
+    $(this).parent('.map__pop-item')
+    .toggleClass('active')
+    .find('.map__pop-cnt').slideToggle();
+  });
+});
+$(function() {
+  if ( $(window).width() < 1280 ) {
+    $('.map__pop-cnt').hide();
+    $('.tabs-acc__btn').on('click', function(){
+      $(this).toggleClass('active').siblings('.tabs-acc__cnt').slideToggle();
+    })
+  };
+});
